7f50cecbcc107049ea78c937922b19618904dccf
Guilherme Schiavone <guilherme-schiavone@hotmail.com>
2021-06-13 16:17:41 -0300

    Implementação inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { Container, Button, TextField } from '@material-ui/core'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container fixed>
+        <TextField id="outlined-basic" label="Usuário" variant="outlined" />
+        <Button variant="contained" color="primary">
+          Default
+        </Button>
+      </Container>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/electron/electronStarter.js
@@ -0,0 +1,28 @@
+require('dotenv/config')
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    path.join(__dirname, '/../build/index.html')
+
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minHeight: 600,
+    minWidth: 800,
+    resizable: false,
+    titleBarStyle: 'hidden'
+  })
+
+  mainWindow.loadURL(startUrl)
+}
+
+app.whenReady().then((_) => {
+  createWindow()
+})
+
+app.on('window-all-closed', () => {
+  app.quit()
+})
